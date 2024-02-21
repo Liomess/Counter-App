8af6e98071cad5d9ddfb95ba77d31cc4abb265ad
Ayush Tiwari <90912985+Liomess@users.noreply.github.com>
2024-02-21 12:10:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const countValue = document.getElementById('counter');
+
+
+// (countValue.innerText) RETURNS A STRING
+
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+
+function decrement(){
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
